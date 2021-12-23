a61c1c4b2da83005c80d92bad71c87868fb84bf7
Valduy <43514596+Valduy@users.noreply.github.com>
2021-12-23 16:59:23 +0300

    Add more tests for AABB

--- UnitTests/GameEngine/Mathematics/MathTests.cs
+++ UnitTests/GameEngine/Mathematics/MathTests.cs
@@ -1,4 +1,6 @@
-﻿using GameEngine.Mathematics;
+﻿using System.Collections;
+using System.Collections.Generic;
+using GameEngine.Mathematics;
 using OpenTK.Mathematics;
 using Xunit;
 
@@ -6,32 +8,70 @@ namespace UnitTests.GameEngine.Mathematics
 {
     public class MathTests
     {
-        [Fact]
-        public void IsAABBIntersects_NotIntersectedAABBs_False()
+        [Theory]
+        [ClassData(typeof(NotIntersectedGenerator))]
+        public void IsAABBIntersects_NotIntersectedAABBs_False(Vector2 position1, Vector2 size1, Vector2 position2, Vector2 size2)
         {
-            var position1 = new Vector2(2.0f, 2.0f);
-            var size1 = new Vector2(4.0f, 2.0f);
-
-            var position2 = new Vector2(3.0f, 4.0f);
-            var size2 = new Vector2(4.0f, 1.0f);
-
             var result = Math.IsAABBIntersects(position1, size1.X, size1.Y, position2, size2.X, size2.Y);
 
             Assert.False(result);
         }
 
-        [Fact]
-        public void IsAABBIntersects_IntersectedAABBs_True()
+        [Theory]
+        [ClassData(typeof(IntersectedGenerator))]
+        public void IsAABBIntersects_IntersectedAABBs_True(Vector2 position1, Vector2 size1, Vector2 position2, Vector2 size2)
         {
-            var position1 = new Vector2(2.0f, 2.0f);
-            var size1 = new Vector2(4.0f, 2.0f);
-
-            var position2 = new Vector2(3.0f, 2.5f);
-            var size2 = new Vector2(4.0f, 2.0f);
-
             var result = Math.IsAABBIntersects(position1, size1.X, size1.Y, position2, size2.X, size2.Y);
 
             Assert.True(result);
         }
+    }
+
+    public class NotIntersectedGenerator : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new Vector2(2.0f, 2.0f),
+                new Vector2(4.0f, 2.0f),
+                new Vector2(3.0f, 4.0f),
+                new Vector2(4.0f, 1.0f),
+            };
+            yield return new object[]
+            {
+                new Vector2(4.0f, 2.0f),
+                new Vector2(5.0f, 2.0f),
+                new Vector2(0.0f, 0.0f),
+                new Vector2(2.0f, 1.0f),
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => GetEnumerator();
+    }
+
+    public class IntersectedGenerator : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new Vector2(2.0f, 2.0f),
+                new Vector2(4.0f, 2.0f),
+                new Vector2(3.0f, 2.5f),
+                new Vector2(4.0f, 2.0f),
+            };
+            yield return new object[]
+            {
+                new Vector2(2.0f, 2.0f),
+                new Vector2(4.0f, 2.0f),
+                new Vector2(2.0f, 2.0f),
+                new Vector2(2.0f, 2.0f),
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => GetEnumerator();
     }
 }
